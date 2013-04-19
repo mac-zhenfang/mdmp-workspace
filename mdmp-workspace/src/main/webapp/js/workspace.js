f5e54daba559364efd44152a436b466145fe6d0f
mac-zhenfang <mac.zhenfang@gmail.com>
2013-04-19 20:19:50 +0800

    Commit

--- mdmp-workspace/src/main/webapp/js/workspace.js
+++ mdmp-workspace/src/main/webapp/js/workspace.js
@@ -51,198 +51,135 @@ Workspace.service('AppService', [ '$http', '$AppListURL',
 				scope.currentApp = scope.apps[0];
 			};
 		} ]);
-Workspace
-		.service(
-				'GroupService',
-				[
-						'$http',
-						'$CategoryListURL',
-						function($http, $CategoryListURL) {
-							// FIXME
-							var callUri = $CategoryListURL
-									+ "?callback=JSON_CALLBACK";
-							this.listGroups = function(scope) {
-								// test
-								console.log(callUri);
-								$http({
-									method : 'jsonp',
-									url : callUri
-								})
-										.success(
-												function(data, status) {
-													// FIXME, left-bar
-													// scope.groups = {
-													// groups : [ {
-													// name : "group 1",
-													// reports : [ {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// } ]
-													// }, {
-													// name : "group 1",
-													// reports : [ {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// } ]
-													// }, {
-													// name : "group 1",
-													// reports : [ {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// } ]
-													// }, {
-													// name : "group 1",
-													// reports : [ {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// }, {
-													// name : "report1"
-													// } ]
-													// } ]
-													// };
-													scope.groups = [
-															{
-																name : "group 1",
-																isFocus : true,
-																reports : [
-																		{
-																			name : "report 1 1 sss",
-																			isFocus : true
-																		},
-																		{
-																			name : "report 1 2",
-																			isFocus : false
-																		},
-																		{
-																			name : "report 1 3",
-																			isFocus : false
-																		},
-																		{
-																			name : "report 1 4",
-																			isFocus : false
-																		} ]
-															},
-															{
-																name : "group 2",
-																isFocus : false,
-																reports : [
-																		{
-																			name : "report 2 1",
-																			isFocus : true
-																		},
-																		{
-																			name : "report 2 2",
-																			isFocus : false
-																		},
-																		{
-																			name : "report 2 3",
-																			isFocus : false
-																		},
-																		{
-																			name : "report 2 4",
-																			isFocus : false
-																		} ]
-															},
-															{
-																name : "group 3",
-																isFocus : false,
-																reports : [
-																		{
-																			name : "report 3 1",
-																			isFocus : false
-																		},
-																		{
-																			name : "report 3 2",
-																			isFocus : false
-																		},
-																		{
-																			name : "report 3 3",
-																			isFocus : false
-																		},
-																		{
-																			name : "report 3 4",
-																			isFocus : false
-																		} ]
-															},
-															{
-																name : "group 4",
-																isFocus : false,
-																reports : [
-																		{
-																			name : "report 4 1",
-																			isFocus : true
-																		},
-																		{
-																			name : "report 4 2",
-																			isFocus : false
-																		},
-																		{
-																			name : "report 4 3",
-																			isFocus : false
-																		},
-																		{
-																			name : "report 4 4",
-																			isFocus : false
-																		} ]
-															} ];
-													console.log(scope.groups);
-													// FIXME panes data;
-													// Generate Reports
-													scope.reports = {
-														focusReport : {},
-														rows : []
-													};
-													for (index in scope.groups) {
-														var group = scope.groups[index];
-														if (group.isFocus == true) {
-															for (rep in group.reports) {
-																var report = group.reports[rep];
-																if (report.isFocus == true) {
-																	scope.reports["focusReport"] = report;
-																} else {
-																	var cols = [];
-																	if (rep % 3 == 0) {
-																		scope.reports.rows[scope.reports.rows.length] = cols;
-																	}
-																	cols[cols.length] = report;
-																}
-															}
-														}
-													}
-												}).error(
-												function(data, status) {
-													console.log(data
-															+ " , status : "
-															+ status);
-												});
-							};
-						} ]);
+Workspace.service('GroupService', [ '$http', '$CategoryListURL',
+		function($http, $CategoryListURL) {
+			// FIXME
+			var callUri = $CategoryListURL + "?callback=JSON_CALLBACK";
+			this.listGroups = function(scope) {
+				// test
+				console.log(callUri);
+				$http({
+					method : 'jsonp',
+					url : callUri
+				}).success(function(data, status) {
+					scope.groups = [ {
+						name : "group 1",
+						isFocus : true,
+						reports : [ {
+							name : "report 1 1 sss",
+							isFocus : true
+						}, {
+							name : "report 1 2",
+							isFocus : false
+						}, {
+							name : "report 1 3",
+							isFocus : false
+						}, {
+							name : "report 1 4",
+							isFocus : false
+						}, {
+							name : "report 1 5",
+							isFocus : false
+						}, {
+							name : "report 1 6",
+							isFocus : false
+						}, {
+							name : "report 1 7",
+							isFocus : false
+						} ]
+					}, {
+						name : "group 2",
+						isFocus : false,
+						reports : [ {
+							name : "report 2 1",
+							isFocus : true
+						}, {
+							name : "report 2 2",
+							isFocus : false
+						}, {
+							name : "report 2 3",
+							isFocus : false
+						}, {
+							name : "report 2 4",
+							isFocus : false
+						}, {
+							name : "report 2 5",
+							isFocus : false
+						}, {
+							name : "report 2 6",
+							isFocus : false
+						}, {
+							name : "report 2 7",
+							isFocus : false
+						} ]
+					}, {
+						name : "group 3",
+						isFocus : false,
+						reports : [ {
+							name : "report 3 1",
+							isFocus : false
+						}, {
+							name : "report 3 2",
+							isFocus : false
+						}, {
+							name : "report 3 3",
+							isFocus : false
+						}, {
+							name : "report 3 4",
+							isFocus : false
+						}, {
+							name : "report 3 5",
+							isFocus : false
+						}, {
+							name : "report 3 6",
+							isFocus : false
+						}, {
+							name : "report 3 7",
+							isFocus : false
+						} ]
+					}, {
+						name : "group 4",
+						isFocus : false,
+						reports : [ {
+							name : "report 4 1",
+							isFocus : true
+						}, {
+							name : "report 4 2",
+							isFocus : false
+						}, {
+							name : "report 4 3",
+							isFocus : false
+						}, {
+							name : "report 4 4",
+							isFocus : false
+						}, {
+							name : "report 4 5",
+							isFocus : false
+						}, {
+							name : "report 4 6",
+							isFocus : false
+						}, {
+							name : "report 4 7",
+							isFocus : false
+						} ]
+					} ];
+					// console.log(scope.groups);
+					// FIXME panes data;
+					// Generate Reports
+					for (index in scope.groups) {
+						var group = scope.groups[index];
+						if (group.isFocus) {
+							scope.initPanesReports(group, scope)
+						}
+						;
+					}
+					;
+					// console.log(scope.reports);
+				}).error(function(data, status) {
+					console.log(data + " , status : " + status);
+				});
+			};
+		} ]);
 // Controller
 var WorkspaceController = function($scope, GroupService, AppService) {
 	$scope.init = function() {
@@ -250,59 +187,80 @@ var WorkspaceController = function($scope, GroupService, AppService) {
 		GroupService.listGroups($scope);
 
 	};
+	$scope.initPanesReports = function(group, scope) {
+		scope.reports = {
+			focusReport : {},
+			rows : []
+		};
+		scope.panes = [{
+			title : group.name
+		}];
+		var cols = [];
+		for (rep in group.reports) {
+			var report = group.reports[rep];
+			if (report.isFocus == true) {
+				scope.reports["focusReport"] = report;
+			} else {
+				if ((rep - 1) % 3 == 0) {
+					cols = [];
+					console.log(" rows " + scope.reports.rows.length);
+					scope.reports.rows[scope.reports.rows.length] = {
+						"cols" : cols
+					};
+				}
+				cols[cols.length] = report;
+			}
+		}
+	};
 	$scope.initApps = function() {
 		// Get userId from cookies
 		AppService.listApps($scope);
 
 	};
-
+	$scope.panes = [];
 	$scope.radioValue = [ " Last 1 day", "Last 1 Week", "Last 1 Month" ];
 
 	// FIXME init the reports for 1 group
-//	$scope.reports = {
-//		focusReport : {
-//			name : "Focus Report"
-//		},
-//		rows : [ {
-//			cols : [ {
-//				name : "report1"
-//			}, {
-//				name : "report12"
-//			}, {
-//				name : "report13"
-//			} ]
-//		}, {
-//			cols : [ {
-//				name : "report21"
-//			}, {
-//				name : "report22"
-//			}, {
-//				name : "report23"
-//			} ]
-//		}, {
-//			cols : [ {
-//				name : "report31"
-//			}, {
-//				name : "report32"
-//			}, {
-//				name : "report33"
-//			} ]
-//		} ]
-//	};
-	// FIXME panes
-	$scope.panes = [ {
-		title : "tab1"
-	}, {
-		title : "tab2"
-	} ];
+	// $scope.reports = {
+	// focusReport : {
+	// name : "Focus Report"
+	// },
+	// rows : [ {
+	// cols : [ {
+	// name : "report1"
+	// }, {
+	// name : "report12"
+	// }, {
+	// name : "report13"
+	// } ]
+	// }, {
+	// cols : [ {
+	// name : "report21"
+	// }, {
+	// name : "report22"
+	// }, {
+	// name : "report23"
+	// } ]
+	// }, {
+	// cols : [ {
+	// name : "report31"
+	// }, {
+	// name : "report32"
+	// }, {
+	// name : "report33"
+	// } ]
+	// } ]
+	// };
 	// AddPane
-	$scope.addPane = function(name) {
-		console.log(name);
+	$scope.addPane = function(index) {
+		var group = $scope.groups[index];
+
+		console.log(group);
 		if ($scope.panes.length < 10) {
-			$scope.panes[$scope.panes.length] = {
-				title : name
-			};
+			$scope.initPanesReports(group, $scope);
+			$scope.panes[$scope.panes.length - 1]["active"] = true;
 		}
+
 	};
 };
 
